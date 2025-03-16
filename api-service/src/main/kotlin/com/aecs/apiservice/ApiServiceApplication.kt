54019c54a2b0a0b1084b608b5301f444b91c7015
Amal Ranasinghe <amal@soho.com.au>
2025-03-16 15:49:33 +0530

    created api gateway module

---
+++ api-service/src/main/kotlin/com/aecs/apiservice/ApiServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.aecs.apiservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiServiceApplication>(*args)
+}
