411dd2605df0d8ed1467fb0aa8ef2ad18e52e46c
Amal Ranasinghe <amal@soho.com.au>
2025-03-16 14:56:54 +0530

    added eureka server under the discovery-server

---
+++ discovery-service/src/test/kotlin/com/aecs/discoveryserver/discoveryservice/DiscoveryServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aecs.discoveryserver.discoveryservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
