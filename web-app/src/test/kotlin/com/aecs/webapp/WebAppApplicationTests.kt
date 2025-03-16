1535a02e6819bae1b1144d99edafec256f50ff4e
Amal Ranasinghe <amal@soho.com.au>
2025-03-16 23:06:02 +0530

    created webb-app

---
+++ web-app/src/test/kotlin/com/aecs/webapp/WebAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aecs.webapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
