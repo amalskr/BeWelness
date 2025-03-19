f8e1dc7f7201733427a660218b26931b0c621c00
Amal Ranasinghe <amal@soho.com.au>
2025-03-19 12:30:36 +0530

    chat close button implemented

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -18,17 +18,28 @@ document.addEventListener("DOMContentLoaded", async function () {
 
     //load chat data and call loadChatHistory
     const chatData = localStorage.getItem('counselor_chat');
+
+    if (!chatData) {
+        window.location.href = '/BeWelness/static/dashboard.html';
+        return;
+    }
+
     if (chatData) {
         //counID ,counName, cusId
         const chatInfo = JSON.parse(chatData);
         document.getElementById('counselorName').textContent = `Chat with Dr.${chatInfo.counName}`;
 
-        await loadChatHistory(chatInfo.cusId,chatInfo.counID);
+        await loadChatHistory(chatInfo.cusId, chatInfo.counID);
     }
+
+    document.getElementById('logoutBtn').addEventListener('click', function() {
+        localStorage.removeItem('counselor_chat');
+        window.history.back();
+    });
 });
 
 //Load Chat history
-async function loadChatHistory(customerId,counselorId) {
+async function loadChatHistory(customerId, counselorId) {
     try {
         const response = await fetch(`http://localhost:8090/message/list?customerId=${customerId}&counselorId=${counselorId}`);
         if (!response.ok) throw new Error("Failed to fetch chat history");
@@ -63,14 +74,14 @@ async function sendMessage() {
         try {
             const response = await fetch('http://localhost:8090/message/send', {
                 method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ customerId, counselorId, content: messageText, type: "SENT" })
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({customerId, counselorId, content: messageText, type: "SENT"})
             });
 
             if (!response.ok) throw new Error("Failed to send message");
 
             input.value = '';
-            await loadChatHistory(customerId,counselorId); // Reload chat history on success
+            await loadChatHistory(customerId, counselorId); // Reload chat history on success
         } catch (error) {
             console.error("Error sending message:", error);
         }
